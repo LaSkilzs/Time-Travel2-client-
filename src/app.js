ad2dc54b5c81bd22d7aa206937a959e2a44fa6cb
La Fountain <github email address>
2019-04-01 09:56:27 -0400

    Complete ui first draft

---
+++ src/app.js
@@ -0,0 +1,8 @@
+const switchhome = document.getElementById("home");
+const switchwork = document.getElementById("findwork");
+const switchprofile = document.getElementById("profile");
+
+let wantads_list = new Array();
+let latest_jobs = new Array();
+let industry_list = new Array();
+let profile_list = new Array();
